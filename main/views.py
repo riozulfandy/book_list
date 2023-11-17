9d1e5c26da7f5961e043cae96f75d9ddab0c9647
Muhammad Mariozulfandy <muhammad.mariozulfandy@ui.ac.id>
2023-11-17 15:59:03 +0700

    Selesai Tugas 9 Django

--- main/views.py
+++ main/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 import datetime  
 
-# Create your views here.
+# Create your views here. 
 @login_required(login_url='/login')
 def show_main(request):
     items = Item.objects.filter(user=request.user)
